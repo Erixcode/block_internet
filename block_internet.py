9961bf59a88eaae7cbd976f60d1d7bfcc239f1fd
ErixPloit <35405767+Erixcode@users.noreply.github.com>
2023-11-17 13:28:04 +0330

    Update block_internet.py
    adding handling of multiprocesses for apps like chrome

--- block_internet.py
+++ block_internet.py
@@ -1,18 +1,32 @@
 import subprocess
 import psutil
 
-def block_internet_access(program_pid):
+def block_internet_access(program_name):
     # Disable firewalld
     subprocess.run("sudo systemctl stop firewalld", shell=True)
 
-    # Block outgoing connections
-    rule_outgoing = f"sudo iptables -A OUTPUT -p tcp --match owner --uid-owner {program_pid} -j DROP"
-    subprocess.run(rule_outgoing, shell=True)
+    # Find all UIDs for the specified program name
+    program_uids = [process.info['username'] for process in psutil.process_iter(['pid', 'name', 'username']) if process.info['name'] == program_name]
 
-def unblock_internet_access(program_pid):
-    # Remove the outgoing rule
-    rule_outgoing = f"sudo iptables -D OUTPUT -p tcp --match owner --uid-owner {program_pid} -j DROP"
-    subprocess.run(rule_outgoing, shell=True)
+    # Block outgoing connections for each UID
+    for uid in program_uids:
+        subprocess.run(f"sudo iptables -A OUTPUT -p tcp --match owner --uid-owner {uid} -j DROP", shell=True)
+
+    # Block incoming connections for each UID
+    for uid in program_uids:
+        subprocess.run(f"sudo iptables -A INPUT -p tcp --match owner --uid-owner {uid} -j DROP", shell=True)
+
+def unblock_internet_access(program_name):
+    # Find all UIDs for the specified program name
+    program_uids = [process.info['username'] for process in psutil.process_iter(['pid', 'name', 'username']) if process.info['name'] == program_name]
+
+    # Remove outgoing rules for each UID
+    for uid in program_uids:
+        subprocess.run(f"sudo iptables -D OUTPUT -p tcp --match owner --uid-owner {uid} -j DROP", shell=True)
+
+    # Remove incoming rules for each UID
+    for uid in program_uids:
+        subprocess.run(f"sudo iptables -D INPUT -p tcp --match owner --uid-owner {uid} -j DROP", shell=True)
 
     # Enable firewalld back
     subprocess.run("sudo systemctl start firewalld", shell=True)
@@ -25,28 +39,16 @@ def main():
     # Example: Get the program name from the user
     program_name = get_user_input()
 
-    # Example: Check if the program is running and get its PID
-    program_pid = None
-    for process in psutil.process_iter(['pid', 'name']):
-        if process.info['name'] == program_name:
-            program_pid = process.info['pid']
-            break
+    # Example: Check if the program is running and block internet access
+    block_internet_access(program_name)
 
-    if program_pid is not None:
-        print(f"The PID of {program_name} is: {program_pid}")
+    # Wait for user input before unblocking and exiting
+    input("Press Enter to unblock and exit...")
 
-        # Example: Block internet access for the specified program
-        block_internet_access(program_pid)
+    # Remove the firewall rules after user input
+    unblock_internet_access(program_name)
 
-        # Wait for user input before exiting
-        input("Press Enter to exit...")
-
-        # Remove the firewall rule after your program completes
-        unblock_internet_access(program_pid)
-
-        print("Script completed. restoring settings")
-    else:
-        print(f"{program_name} is not running.")
+    print("Script completed.")
 
 if __name__ == "__main__":
     main()
